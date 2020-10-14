064ffec878c2897102f1d361ef87bbd640b2fcdd
t-iwashiro <t_iwashiro@waku-2.com>
2020-10-15 01:56:49 +0900

    stylelintとhuskyの機能を拡張

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     '@typescript-eslint',
     'import',
     'jsx-a11y',
+    'prefer-arrow',
     'prettier',
     'react',
     'react-hooks',
@@ -90,6 +91,14 @@ module.exports = {
         html: 'enforce',
         custom: 'enforce',
         explicitSpread: 'ignore',
+      },
+    ],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
       },
     ],
   },
