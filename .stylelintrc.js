f3f1026b937f4bf569177e95719a7b3f726e3db9
iwashiro-takuya <gutlo.ol0414@gmail.com>
2020-12-21 00:07:06 +0900

    stylelint.jsをlint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-  ],
-  plugins: [
-    'stylelint-order'
-  ],
-  ignoreFiles: [
-    '**/node_modules/**'
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
   rules: {
     'string-quotes': 'single',
   },
