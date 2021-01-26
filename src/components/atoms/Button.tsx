8b574c1704c23f323c9922f9a2cef19dac65a931
Takuya_Iwashiro <gutlo.ol0414@gmail.com>
2021-01-26 17:15:15 +0900

    Add style to Game start button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
+import Const from 'src/const';
+
+const { Color } = Const;
 
 export interface ButtonProp {
   onClick: () => void;
@@ -6,9 +10,18 @@ export interface ButtonProp {
 }
 const Button: React.FC<ButtonProp> = ({ children, onClick, dataCy }) => {
   return (
-    <button type="button" onClick={onClick} data-cy={dataCy}>
+    <StyledBottun type="button" onClick={onClick} data-cy={dataCy}>
       {children}
-    </button>
+    </StyledBottun>
   );
 };
 export default Button;
+
+const StyledBottun = styled.button`
+  display: block;
+  padding: 8px 16px;
+  margin: 0 auto;
+  background-color: ${Color.BG_WHITE};
+  border: 1px solid ${Color.BD_BLACK};
+  border-radius: 4px;
+`;
