34af00abd25adcd009e65ad46b4c2fc9f9774cdb
Takuya_Iwashiro <gutlo.ol0414@gmail.com>
2021-02-09 01:40:12 +0900

    Add Square Component storybook

--- src/components/atoms/Square/Square.stories.tsx
+++ src/components/atoms/Square/Square.stories.tsx
@@ -1,19 +1,12 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
 import { Story } from '@storybook/react/types-6-0';
-import Square from './component';
+import Square, { SquareProp } from './component';
 
 export default {
   title: 'Square',
   component: Square,
 };
 
-const Template: Story<ComponentProps<typeof Square>> = (args) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Square {...args} />
-);
+const Template: Story<SquareProp> = () => <Square dataCy="dataCy" />;
 
-export const FirstStory = Template.bind({});
-
-FirstStory.args = {
-  dataCy: 'dataCy',
-};
+export const normal = Template.bind({});
