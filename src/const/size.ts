3eb994e46fa550e055f24a05208dc978e77c8aff
Takuya_Iwashiro <gutlo.ol0414@gmail.com>
2021-02-27 16:08:55 +0900

    make Information Component

--- src/const/size.ts
+++ src/const/size.ts
@@ -1,4 +1,10 @@
 export default {
   // Size of one side of a square(px)
-  SQUARE_SIZE: 80,
+  SQUARE_SIZE: 64,
+
+  // Size of text
+  FS_16: '16px',
+  FS_20: '20px',
+  FS_24: '24px',
+  FS_32: '32px',
 } as const;
