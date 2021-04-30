f053678891851085679fe98734ce7f5f2c2df5fe
Takuya_Iwashiro <gutlo.ol0414@gmail.com>
2021-04-30 12:10:46 +0900

    add Player Information unit test

---
+++ src/components/organisms/score/player-information/index.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Const from 'src/const';
+import PlayerInformation from '.';
+
+const { Player } = Const;
+
+describe('Square unit test', () => {
+  it('snapshot(Player White)', () => {
+    const component = renderer.create(
+      <PlayerInformation player={Player.WHITE} />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it('snapshot(Player Black)', () => {
+    const component = renderer.create(
+      <PlayerInformation player={Player.BLACK} />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
