429db2f410d519a8e5c4f759cb7a6f2366409149
t-i-0414 <gutlo.ol0414@gmail.com>
2023-05-31 16:18:12 +0900

    fix domain

--- src/domains/index.ts
+++ src/domains/index.ts
@@ -1,12 +1,36 @@
+import {
+  createBoard,
+  turnOverPieces,
+  updateBoardByNextTurnPiece,
+  getSquaresHavingCanTurnOverPieceByEachDirection,
+  getBoardSideSquareCount,
+} from './board/service';
+import {
+  getNextTurnPiece,
+  getScore,
+  getResult,
+  isGameFinished,
+} from './game/service';
+
 export type * from './board/interface';
 export type * from './game/interface';
 export type * from './piece/interface';
 export type * from './player/interface';
 export type * from './square/interface';
-
-export * as BoardDomainService from './board/service';
-export * as GameDomainService from './game/service';
-
 export * as BoardMock from './board/mock';
 export * as GameMock from './game/mock';
 export * as PlayerMock from './player/mock';
+
+export const BoardDomainService = {
+  createBoard,
+  turnOverPieces,
+  updateBoardByNextTurnPiece,
+  getSquaresHavingCanTurnOverPieceByEachDirection,
+  getBoardSideSquareCount,
+};
+export const GameDomainService = {
+  getNextTurnPiece,
+  getScore,
+  getResult,
+  isGameFinished,
+};
