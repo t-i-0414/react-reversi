7d881b72c2986a4999df3773edb83fca31d1dc9e
t-i-0414 <gutlo.ol0414@gmail.com>
2023-06-01 08:02:27 +0900

    add test

--- src/domains/game/service.test.ts
+++ src/domains/game/service.test.ts
@@ -1,8 +1,14 @@
 import { expect, it } from '@jest/globals';
+import { Board } from '../board/interface';
 import { PieceColor } from '../piece/interface';
 import { Game } from './interface';
 import { game as gameMock } from './mock';
-import { getResult, getScore, getNextTurnPiece } from './service';
+import {
+  getResult,
+  getScore,
+  getNextTurnPiece,
+  isGameFinished,
+} from './service';
 
 describe('gameDomainService', () => {
   describe('getResult', () => {
@@ -130,5 +136,96 @@ describe('gameDomainService', () => {
         expect(result).toBe(expected);
       },
     );
+  });
+
+  describe('isGameFinished', () => {
+    const testCases: {
+      case: string;
+      board: Board;
+      expected: boolean;
+    }[] = [
+      {
+        case: 'when blackPiece win',
+        board: [
+          {
+            key: '0-0',
+            column: 0,
+            row: 0,
+            piece: 'black',
+          },
+        ],
+        expected: true,
+      },
+      {
+        case: 'when whitePiece win',
+        board: [
+          {
+            key: '0-0',
+            column: 0,
+            row: 0,
+            piece: 'white',
+          },
+        ],
+        expected: true,
+      },
+      {
+        case: 'when blackPiece only',
+        board: [
+          {
+            key: '0-0',
+            column: 0,
+            row: 0,
+            piece: 'black',
+          },
+          {
+            key: '1-0',
+            column: 1,
+            row: 0,
+            piece: null,
+          },
+        ],
+        expected: true,
+      },
+      {
+        case: 'when whitePiece only',
+        board: [
+          {
+            key: '0-0',
+            column: 0,
+            row: 0,
+            piece: 'white',
+          },
+          {
+            key: '1-0',
+            column: 1,
+            row: 0,
+            piece: null,
+          },
+        ],
+        expected: true,
+      },
+      {
+        case: 'when the game is in progress',
+        board: gameMock.board,
+        expected: false,
+      },
+    ];
+    it.each(testCases)(
+      'should return $expected ($case)',
+      ({ board, expected }: { board: Board; expected: boolean }) => {
+        const game: Game = structuredClone(gameMock);
+        game.board = board;
+
+        const result = isGameFinished(game);
+
+        expect(result).toBe(expected);
+      },
+    );
+
+    it('should return false', () => {
+      const result = isGameFinished(gameMock);
+
+      expect(result).toBe(false);
+    });
   });
 });
