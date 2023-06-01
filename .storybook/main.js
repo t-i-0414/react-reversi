7cdbc4ca779fd18d5051f628f9ed9429549a91fd
t-i-0414 <gutlo.ol0414@gmail.com>
2023-06-02 08:09:07 +0900

    build: webpack→vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  stories: [
-    // '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
